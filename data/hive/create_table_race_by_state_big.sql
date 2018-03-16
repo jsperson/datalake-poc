9c646999088ab3600ea28e8f50f34afa204f8d01
jsperson <jsperson@gmail.com>
2018-03-16 20:03:03 +0000

    Updated reports and various other items

--- data/hive/create_table_race_by_state_big.sql
+++ data/hive/create_table_race_by_state_big.sql
@@ -1,4 +1,4 @@
-CREATE EXTERNAL TABLE race_by_state(
+CREATE EXTERNAL TABLE race_by_state_big(
    location varchar(50)
 ,  white decimal(3,2)
 ,  black decimal(3,2)
@@ -8,11 +8,12 @@ CREATE EXTERNAL TABLE race_by_state(
 ,  pacific_islander decimal(3,2)
 ,  two_or_more decimal(3,2)
 ,  total decimal(3,2)
+, as_of_year string
 )
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 LOCATION
-  's3://datalake-poc-data/users/sperson/race_by_state'
+  's3://datalake-poc-data/users/sperson/race_by_state_big/csv'
 TBLPROPERTIES (
   'skip.header.line.count'='1')
