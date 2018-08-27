c75196f8bafb1415880a4657a4299df78c347b6e
Matt Roberts <vtpanda@users.noreply.github.com>
2018-08-27 14:36:17 -0400

    Updated scripts to change feed name to hmda_lar.

--- etl/hmda_template/HMDAFileIngestionDataPipelines_CreateStack.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-aws cloudformation create-stack --stack-name HMDADataIngestionDataPipelines --template-url https://s3.amazonaws.com/datalake-poc-matt/feeds/hmda/hmda_lar/scripts/deployment/HMDAFileIngestionDataPipelines.yaml --parameters file://HMDAFileIngestionDataPipelines_CreateStack_Parameters.json --profile datalake
